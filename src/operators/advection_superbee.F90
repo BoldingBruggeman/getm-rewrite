a709fa7f68956ad1457954bdcab3a866d12259e7
Karsten Bolding <karsten@bolding-bruggeman.com>
2020-09-09 13:15:54 +0200

    templating advection schemes

---
+++ src/operators/advection_superbee.F90
@@ -0,0 +1,10 @@
+#define _SCHEME_ SUPERBEE
+#define _LIMITER1_ limiter = MAX( MIN( 2*ratio , 1.0_real64 ) , MIN( ratio , 2.0_real64 ) )
+#define _LIMITER2_
+#define _LIMITER3_
+#define _LIMITER4_
+
+#define _U_SUB_NAME_ u_advection_superbee
+#define _V_SUB_NAME_ v_advection_superbee
+
+#include "advection.F90.template"
---
+++ src/operators/advection_upstream.F90
@@ -0,0 +1,10 @@
+#define _SCHEME_ UPSTREAM
+#define _LIMITER1_ limiter = 0._real64
+#define _LIMITER2_
+#define _LIMITER3_
+#define _LIMITER4_
+
+#define _U_SUB_NAME_ u_advection_upstream
+#define _V_SUB_NAME_ v_advection_upstream
+
+#include "advection.F90.template"
