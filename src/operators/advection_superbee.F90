b0ea4a52e4215e560d606785fa8a34376f968225
Knut <knutaros@gmx.net>
2020-10-15 11:46:09 +0200

    some cosmetics for superbee

--- src/operators/advection_superbee.F90
+++ src/operators/advection_superbee.F90
@@ -7,6 +7,6 @@
 #define _MODULE_NAME_ advection_superbee
 #define _U_SUB_NAME_ u_advection_superbee
 #define _V_SUB_NAME_ v_advection_superbee
-#define _W_SUB_NAME_ W_advection_superbee
+#define _W_SUB_NAME_ w_advection_superbee
 
 #include "advection.F90.template"
