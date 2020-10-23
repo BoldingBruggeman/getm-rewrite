72185fef4910c2eaaba814b29da168a249f7d91f
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2020-10-23 15:02:39 +0100

    implemented gather

--- python/examples/test_parallel.py
+++ python/examples/test_parallel.py
@@ -1,13 +1,18 @@
+import numpy
+from mpi4py import MPI
 import pygetm.parallel
 
-import numpy
+size = MPI.COMM_WORLD.Get_size()
+assert size == 4, 'This example must be run on 4 nodes (found %i)' % size
 
-halo = 2
-plusint = 1
+halo = 1
+plusint = 0
 t = pygetm.parallel.Tiling(2, 2)
-f = numpy.empty((3 * halo + plusint, 3 * halo + plusint))
+f = numpy.empty((2, 3 * halo + plusint, 3 * halo + plusint))
 f[...] = -1
 f[..., halo:-halo, halo:-halo] = t.rank
 exf = t.setup(f, halo)
-exf.exchange()
+exf.update_halos()
 print(t.rank, f)
+
+print(t.rank, exf.gather())
--- python/pygetm/parallel.py
+++ python/pygetm/parallel.py
@@ -1,8 +1,33 @@
 from mpi4py import MPI
 import numpy
 
-class ExchangedArray:
+class Tiling:
+    def __init__(self, nx, ny, comm=MPI.COMM_WORLD):
+        def get_rank(i, j):
+            if i >= 0 and i < nx and j >= 0 and j < ny:
+                return self.map[i, j]
+
+        self.comm = comm
+        self.rank = self.comm.Get_rank()
+        self.map = numpy.arange(nx * ny).reshape(nx, ny)
+
+        irow, icol = divmod(self.rank, nx)
+
+        self.top = get_rank(irow - 1, icol)
+        self.bottom = get_rank(irow + 1, icol)
+        self.left = get_rank(irow, icol - 1)
+        self.right = get_rank(irow, icol + 1)
+        self.topleft = get_rank(irow - 1, icol - 1)
+        self.topright = get_rank(irow - 1, icol + 1)
+        self.bottomleft = get_rank(irow + 1, icol - 1)
+        self.bottomright = get_rank(irow + 1, icol + 1)
+
+    def setup(self, field, halo):
+        return DistributedArray(self, field, halo)
+
+class DistributedArray:
     def __init__(self, tiling, field, halo):
+        self.tiling = tiling
         self.comm = tiling.comm
 
         self.tasks = []
@@ -19,36 +44,33 @@ class ExchangedArray:
         add_task(tiling.topright, field[..., halo:halo*2, -halo*2:-halo], field[..., :halo, -halo:])
         add_task(tiling.bottomleft, field[..., -halo*2:-halo, halo:halo*2], field[..., -halo:, :halo])
         add_task(tiling.bottomright, field[..., -halo*2:-halo, -halo*2:-halo], field[..., -halo:, -halo:])
+        self.interior = field[..., halo:-halo, halo:-halo]
 
-    def exchange(self):
+    def update_halos(self):
         Irecv, Isend = self.comm.Irecv, self.comm.Isend
         reqs = []
         for neigbor, inner, outer, cache in self.tasks:
             reqs.append(Irecv(cache, neigbor))
         for neigbor, inner, outer, cache in self.tasks:
-            reqs.append(Isend(inner.reshape(-1, order='A'), neigbor))
+            reqs.append(Isend(numpy.ascontiguousarray(inner), neigbor))
         MPI.Request.Waitall(reqs)
         for neigbor, inner, outer, cache in self.tasks:
             outer[...] = cache
 
-class Tiling:
-    def __init__(self, nx, ny):
-        def get_rank(i, j):
-            if i >= 0 and i < nx and j >= 0 and j < ny:
-                return i * ny + j
+    def gather(self, root=0):
+        rankmap = self.tiling.map
+        rank = self.comm.Get_rank()
+        sendbuf = numpy.ascontiguousarray(self.interior)
+        recvbuf = None
+        if rank == root:
+            recvbuf = numpy.empty((rankmap.size,) + self.interior.shape, dtype=self.interior.dtype)
+        self.comm.Gather(sendbuf, recvbuf, root=root)
+        if rank == root:
+            nx, ny = rankmap.shape
+            ni, nj = recvbuf.shape[-2:]
+            out = numpy.empty(recvbuf.shape[1:-2] + (nx * ni, ny * nj), dtype=recvbuf.dtype)
+            for i in range(nx):
+                for j in range(ny):
+                    out[..., i * ni:(i + 1) * ni, j * nj:(j + 1) * nj] = recvbuf[rankmap[i, j], ...]
+            return out
 
-        self.comm = MPI.COMM_WORLD
-        self.rank = self.comm.Get_rank()
-        irow, icol = divmod(self.rank, nx)
-
-        self.top = get_rank(irow - 1, icol)
-        self.bottom = get_rank(irow + 1, icol)
-        self.left = get_rank(irow, icol - 1)
-        self.right = get_rank(irow, icol + 1)
-        self.topleft = get_rank(irow - 1, icol - 1)
-        self.topright = get_rank(irow - 1, icol + 1)
-        self.bottomleft = get_rank(irow + 1, icol - 1)
-        self.bottomright = get_rank(irow + 1, icol + 1)
-
-    def setup(self, field, halo):
-        return ExchangedArray(self, field, halo)
