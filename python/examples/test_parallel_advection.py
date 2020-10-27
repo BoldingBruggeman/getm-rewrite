2a69e1e179df19beb05600f3fba0f974f5447729
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2020-10-27 13:49:53 +0000

    cleaned up test_parallel_advection

--- python/examples/test_parallel_advection.py
+++ python/examples/test_parallel_advection.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy
 import pygetm
 import pygetm.parallel
@@ -5,24 +7,33 @@ from mpi4py import MPI
 
 import matplotlib.pyplot
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--debug', action='store_true', help='produce plots per subdomain')
+parser.add_argument('-i', '--interval', type=int, help='number of timesteps between plots', default=10)
+parser.add_argument('--nrow', type=int, help='number of rows in subdomain division', default=2)
+parser.add_argument('--ncol', type=int, help='number of columns in subdomain division', default=2)
+args = parser.parse_args()
+
 halo = 2
 Lx, Ly = 100., 100.
 nlev = 1
 
-domain_shape = 400, 600  # nj, ni
+domain_shape = 600, 602  # nj, ni
 
 rank = MPI.COMM_WORLD.Get_rank()
 
 if rank == 0:
-    # Set up global 2D coordinate arrays and bathymetry
+    # Set up global 2D coordinate arrays, bathymetry and initial tracer field
     c1_glob = numpy.broadcast_to(numpy.linspace(-Lx/2, Lx/2, domain_shape[1])[None, :], domain_shape)
     c2_glob = numpy.broadcast_to(numpy.linspace(-Ly/2, Ly/2, domain_shape[0])[:, None], domain_shape)
     H_glob = numpy.zeros(domain_shape)
     H_glob[1:-1, 1:-1] = 1.
+    f_glob = numpy.zeros(domain_shape)
+    f_glob[int(0.2 * domain_shape[-2]):int(0.4 * domain_shape[-2]), int(0.2 * domain_shape[-1]):int(0.4 * domain_shape[-1])] = 5.
 else:
-    c1_glob, c2_glob, H_glob, mask_glob = None, None, None, None
+    c1_glob, c2_glob, H_glob, mask_glob, f_glob = None, None, None, None, None
 
-tiling = pygetm.parallel.Tiling(2, 2)
+tiling = pygetm.parallel.Tiling(args.nrow, args.ncol)
 
 # Set up local subdomain 
 domain = pygetm.Domain(1, nlev, 1, domain_shape[0] // tiling.nrow, 1, domain_shape[1] // tiling.ncol)
@@ -31,7 +42,7 @@ halo = domain.halo
 # Scatter global bathymetry to subdomains
 tiling.wrap(domain.T.H_, halo=halo).scatter(H_glob)
 
-# Scatter global coordinates to subdomains and extract 1D c1, c2
+# Scatter global 2D coordinates to subdomains and extract 1D c1, c2
 c1_ = numpy.zeros_like(domain.T.H_)
 c2_ = numpy.zeros_like(domain.T.H_)
 tiling.wrap(c1_, halo=halo).scatter(c1_glob)
@@ -42,7 +53,7 @@ domain.T.c2_[:] = c2_[:, halo]
 # Infer local mask from bathymetry
 domain.T.mask_[...] = domain.T.H_ == 1.
 
-#print(rank, domain.T.c1_.min(), domain.T.c1_.max(), domain.T.c2_.min(), domain.T.c2_.max())
+#print(rank, domain.T.c1_[0], domain.T.c1_[-1], domain.T.c2_[0], domain.T.c2_[-1])
 
 domain.initialize()
 
@@ -72,22 +83,26 @@ tiling.wrap(domain.V.H_, halo=halo).update_halos()
 tiling.wrap(domain.U.D_, halo=halo).update_halos()
 tiling.wrap(domain.V.D_, halo=halo).update_halos()
 
-u, u_ = domain.array(fill=0.)
-v, v_ = domain.array(fill=0.)
-var, var_ = domain.array(fill=1.)
-u_[:, :] = -omega * c2_
-v_[:, :] = omega * c1_
+# These extra halo updates should not be needed - just for testing
+# tiling.wrap(u_, halo=halo).update_halos()
+# tiling.wrap(v_, halo=halo).update_halos()
+# tiling.wrap(domain.T.mask_, halo=halo).update_halos()
+# tiling.wrap(domain.U.dx_, halo=halo).update_halos()
+# tiling.wrap(domain.V.dx_, halo=halo).update_halos()
+# tiling.wrap(domain.T.dx_, halo=halo).update_halos()
+# tiling.wrap(domain.U.dy_, halo=halo).update_halos()
+# tiling.wrap(domain.V.dy_, halo=halo).update_halos()
+# tiling.wrap(domain.T.dy_, halo=halo).update_halos()
+
+f, f_ = domain.array(fill=0.)
+u_ = -omega * c2_
+v_ = omega * c1_
 u_[domain.U.mask_ == 0] = 0.
 v_[domain.V.mask_ == 0] = 0.
-if rank == 0:
-    var[int(tiling.nrow * 0.2 * domain.shape[1]):int(tiling.nrow * 0.4 * domain.shape[1]), int(tiling.ncol * 0.2 * domain.shape[2]):int(tiling.ncol * 0.4 * domain.shape[2])] = 5.
 
-# These extra halo updates should not be needed - just for testing
-tiling.wrap(u_, halo=halo).update_halos()
-tiling.wrap(v_, halo=halo).update_halos()
-tiling.wrap(domain.U.mask_, halo=halo).update_halos()
-tiling.wrap(domain.V.mask_, halo=halo).update_halos()
-tiling.wrap(domain.T.mask_, halo=halo).update_halos()
+# Wrap tracer for halo updates
+distf = tiling.wrap(f_, halo=halo)
+distf.scatter(f_glob)
 
 # Gather and plot global velocities
 u_glob = tiling.wrap(u_, halo=halo).gather()
@@ -97,52 +112,53 @@ if u_glob is not None:
     ax = fig.gca()
     ax.quiver(u_glob[::10, ::10], v_glob[::10, ::10], angles='xy')
     fig.savefig('vel.png')
-fig = matplotlib.pyplot.figure()
-ax = fig.gca()
-ax.quiver(u_[::10, ::10], v_[::10, ::10], angles='xy')
-fig.savefig('vel_%i.png' % rank)
 
-# Wrap tracer for halo updates
-distvar = tiling.wrap(var_, halo=halo)
-
-# Set up figure for plotting tracer per subdomain
-fig_sub = matplotlib.pyplot.figure()
-ax_sub = fig_sub.gca()
-pc_sub = ax_sub.pcolormesh(var_)
-cb_sub = fig_sub.colorbar(pc_sub)
-
-# Set up figure for plotting global tracer field
-var_glob = distvar.gather()
-if var_glob is not None:
+if args.debug:
+    # Plot local velocities
     fig = matplotlib.pyplot.figure()
     ax = fig.gca()
-    pc = ax.pcolormesh(var_glob)
+    ax.quiver(u_[::10, ::10], v_[::10, ::10], angles='xy')
+    fig.savefig('vel_%i.png' % rank)
+
+    # Set up figure for plotting tracer per subdomain
+    fig_sub = matplotlib.pyplot.figure()
+    ax_sub = fig_sub.gca()
+    pc_sub = ax_sub.pcolormesh(f_)
+    cb_sub = fig_sub.colorbar(pc_sub)
+
+# Set up figure for plotting global tracer field
+if f_glob is not None:
+    fig = matplotlib.pyplot.figure()
+    ax = fig.gca()
+    pc = ax.pcolormesh(f_glob)
     cb = fig.colorbar(pc)
 
-adv = pygetm.Advection(domain, scheme=1)
+adv = pygetm.Advection(domain, scheme=6)
 ifig = 0
 for i in range(Nmax):
-    if i % 10 == 0:
-        # Print tracer max along boundaries, inside and outsde halo 
-        print(i, rank, 'inside', var[0, :].max(), var[-1, :].max(), var[:, 0].max(), var[:, -1].max(), flush=True)
-        print(i, rank, 'outside', rank, var_[halo-1, :].max(), var_[-halo, :].max(), var_[:, halo-1].max(), var_[:, -halo].max(), flush=True)
+    if i % args.interval == 0:
+        if args.debug:
+            # Print tracer max along boundaries, inside and outsde halo 
+            print(i, rank, 'inside', f[0, :].max(), f[-1, :].max(), f[:, 0].max(), f[:, -1].max(), flush=True)
+            print(i, rank, 'outside', rank, f_[halo-1, :].max(), f_[-halo, :].max(), f_[:, halo-1].max(), f_[:, -halo].max(), flush=True)
 
-        # Plot local tracer field
-        pc_sub.set_array(var_.ravel())
-        fig_sub.savefig('subadv_%i_%04i.png' % (rank, ifig))
+            # Plot local tracer field
+            pc_sub.set_array(f_.ravel())
+            fig_sub.savefig('subadv_%i_%04i.png' % (rank, ifig))
 
         # Gather and plot global tracer field
-        var_glob = distvar.gather()
-        if var_glob is not None:
-            pc.set_array(var_glob.ravel())
+        f_glob = distf.gather()
+        if f_glob is not None:
+            print('time step %i of %i, tracer sum = %s' % (i, Nmax, f_glob.sum()), flush=True)
+            pc.set_array(f_glob.ravel())
             fig.savefig('adv_%04i.png' % ifig)
 
         ifig += 1
 
     # Advect
-    adv.calculate(u_, v_, timestep, var_)
+    adv.calculate(u_, v_, timestep, f_)
 
     # Update halos
-    distvar.update_halos()
+    distf.update_halos()
 
 
