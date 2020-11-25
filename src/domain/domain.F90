a4bea2f9a6b791544352e72c48fd0ba9d3fd1fdc
Karsten Bolding <karsten@bolding-bruggeman.com>
2020-11-25 15:14:59 +0100

    domain: reorder initialization - calculate slow elevation using masks

--- src/domain/domain.F90
+++ src/domain/domain.F90
@@ -301,6 +301,8 @@ SUBROUTINE domain_initialize(self)
    if (associated(self%logs)) call self%logs%info('domain_initialize()',level=1)
 
    call self%metrics()
+   call self%uvx_depths()
+   call self%register()
    where (self%T%mask > 0)
       self%T%z = 0._real64
       self%T%zo = 0._real64
@@ -319,8 +321,6 @@ SUBROUTINE domain_initialize(self)
       self%V%ssen = 0._real64
       self%V%sseo = 0._real64
    end where
-   call self%uvx_depths()
-   call self%register()
    call self%cfl_check()
    call self%init_vertical()
    call self%do_vertical()
@@ -415,19 +415,23 @@ SUBROUTINE start_3d(self)
    self%T%ssen=self%T%z
 
    self%U%sseo=self%U%ssen
-   do j=self%U%l(2),self%U%l(2)
-      do i=self%U%l(1),self%U%l(1)-1
-         self%U%ssen(i,j)=0.25*(self%T%sseo(i,j)+self%T%sseo(i+1,j)+self%T%ssen(i,j)+self%T%ssen(i+1,j))
-         self%U%ssen(i,j)=max(self%U%ssen(i,j),-self%U%H(i,j)+self%Dmin)
+   do j=self%U%l(2),self%U%u(2)
+      do i=self%U%l(1),self%U%u(1)-1
+         if (self%U%mask(i,j) > 0) then
+            self%U%ssen(i,j)=0.25*(self%T%sseo(i,j)+self%T%sseo(i+1,j)+self%T%ssen(i,j)+self%T%ssen(i+1,j))
+            self%U%ssen(i,j)=max(self%U%ssen(i,j),-self%U%H(i,j)+self%Dmin)
+         end if
       end do
    end do
    self%U%ho=self%U%hn
 
    self%V%sseo=self%V%ssen
-   do j=self%V%l(2),self%V%l(2)-1
-      do i=self%V%l(1),self%V%l(1)
-         self%V%ssen(i,j)=0.25*(self%T%sseo(i,j)+self%T%sseo(i,j+1)+self%T%ssen(i,j)+self%T%ssen(i,j+1))
-         self%V%ssen(i,j)=max(self%V%ssen(i,j),-self%U%H(i,j)+self%Dmin)
+   do j=self%V%l(2),self%V%u(2)-1
+      do i=self%V%l(1),self%V%u(1)
+         if (self%V%mask(i,j) > 0) then
+            self%V%ssen(i,j)=0.25*(self%T%sseo(i,j)+self%T%sseo(i,j+1)+self%T%ssen(i,j)+self%T%ssen(i,j+1))
+            self%V%ssen(i,j)=max(self%V%ssen(i,j),-self%U%H(i,j)+self%Dmin)
+         end if
       end do
    end do
    self%V%ho=self%V%hn
