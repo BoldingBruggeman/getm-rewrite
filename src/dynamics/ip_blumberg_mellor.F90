008de7225f88017e8273aa84f0b1f62e666d0063
Karsten Bolding <karsten@bolding-bruggeman.com>
2020-12-03 16:59:49 +0100

    internal pressure: blumberg_mellor clean up - timing enabled

--- src/dynamics/ip_blumberg_mellor.F90
+++ src/dynamics/ip_blumberg_mellor.F90
@@ -17,7 +17,7 @@ MODULE SUBROUTINE blumberg_mellor(self,buoy)
 !  Subroutine arguments
    class(type_getm_pressure), intent(inout) :: self
 #define _T3_ self%domain%T%l(1):,self%domain%T%l(2):,self%domain%T%l(3):
-   real(real64), intent(in) :: buoy(:,:,:)
+   real(real64), intent(in) :: buoy(_T3_)
 #undef _T3_
 
 !  Local constants
@@ -26,12 +26,16 @@ MODULE SUBROUTINE blumberg_mellor(self,buoy)
    integer :: i,j,k
    real(real64) :: dxm1,dym1
    real(real64) :: grdl,grdu,buoyl,buoyu,prgr,dxz,dyz
+   real(real64) :: U_time,V_time
 !-----------------------------------------------------------------------
    TGrid: associate( TG => self%domain%T )
+
    UGrid: associate( UG => self%domain%U )
+   Ublock: block
+   real(real64) :: U_start, U_stop
+   call cpu_time(U_start)
    do j=UG%jmin,UG%jmax
       do i=UG%imin,UG%imax
-         self%idpdx(i,j,0) = 0._real64
          if (UG%mask(i,j) > 0) then
             dxm1=1._real64/UG%dx(i,j)
             grdl=(buoy(i+1,j,UG%kmax)-buoy(i,j,UG%kmax))*dxm1
@@ -43,7 +47,6 @@ MODULE SUBROUTINE blumberg_mellor(self,buoy)
                grdl=(buoy(i+1,j,k)-buoy(i,j,k))*dxm1
                buoyu=buoyl
                buoyl=0.5_real64*(buoy(i+1,j,k)+buoy(i,j,k))
-write(*,*) i,j,k,dxm1
                dxz=(TG%zf(i+1,j,k)-TG%zf(i,j,k))*dxm1
                prgr=prgr+0.5_real64*(grdu+grdl)*0.5_real64*(UG%hn(i,j,k)+UG%hn(i,j,k+1))-dxz*(buoyu-buoyl)
                self%idpdx(i,j,k)=UG%hn(i,j,k)*prgr
@@ -51,12 +54,18 @@ write(*,*) i,j,k,dxm1
          end if
       end do
    end do
+   call cpu_time(U_stop)
+   U_time=U_time+U_stop-U_start
+   end block UBlock
    end associate UGrid
+return
 
    VGrid: associate( VG => self%domain%V )
+   Vblock: block
+   real(real64) :: V_start, V_stop
+   call cpu_time(V_start)
    do j=VG%jmin,VG%jmax
       do i=VG%imin,VG%imax
-         self%idpdy(i,j,0) = 0._real64
          if (VG%mask(i,j) > 0) then
             dym1 = 1._real64/VG%dy(i,j)
             grdl=(buoy(i,j+1,VG%kmax)-buoy(i,j,VG%kmax))*dym1
@@ -75,8 +84,12 @@ write(*,*) i,j,k,dxm1
          end if
       end do
    end do
+   call cpu_time(V_stop)
+   V_time=V_time+V_stop-V_start
+   end block Vblock
    end associate VGrid
    end associate TGrid
+   write(33,*) U_time,V_time
    end subroutine blumberg_mellor
 
 !---------------------------------------------------------------------------
