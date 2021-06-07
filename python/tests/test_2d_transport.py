e8cd167ff842576ae09e9445d1aaeb6a2cc5593b
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-06-07 08:36:32 +0100

    test_2d_transport: test against analytically expected value

--- python/tests/test_2d_transport.py
+++ python/tests/test_2d_transport.py
@@ -2,30 +2,44 @@ import sys
 import argparse
 
 import numpy
+import matplotlib.pyplot
 
 import pygetm
 import pygetm.domain
 
-def check_range(name, values, rtol=1e-12, atol=1e-12):
+def check_range(name, values, rtol=1e-12, atol=1e-12, target_value=None):
     print('  %s... ' % name, end='', flush=True)
     absrange = values.max() - values.min()
     mean = values.mean()
     crit = rtol * abs(mean) + atol
     valid = absrange < crit
+    result = 'OK' if valid else 'FAILED because field is not homogeneous'
     relrange = 0 if absrange == 0 else absrange / abs(mean)
-    print('%s (mean %.6g, spread %.6g, relative spread %.6g)' % ('OK' if valid else 'FAILED', mean, absrange, relrange))
+    if valid and target_value is not None:
+        valid = abs(mean - target_value) < rtol * abs(target_value) + atol
+        if not valid:
+            result = 'FAILED to match target value %.6g' % target_value
+    print('%s (mean %.6g, spread %.6g, relative spread %.6g)' % (result, mean, absrange, relrange))
+    if not valid:
+        plot('%s.png' % name, values)
     return valid
+
+def plot(name, values):
+    fig, ax = matplotlib.pyplot.subplots()
+    pc = ax.pcolormesh(values)
+    fig.colorbar(pc)
+    fig.savefig(name, dpi=300)
 
 def test(name, periodic_x=False, periodic_y=False, tau_x=0., tau_y=0., timestep=10., ntime=360, apply_bottom_friction=False):
     print('%s...' % name, flush=True)
 
-    # Set up rectangular domain with outer points masked
+    # Set up rectangular domain (all points unmasked)
     domain = pygetm.domain.Domain.create_cartesian(500.*numpy.arange(100), 500.*numpy.arange(30), 1, f=0, H=50, periodic_x=periodic_x, periodic_y=periodic_y)
     sim = pygetm.Simulation(domain, runtype=1, advection_scheme=1, apply_bottom_friction=apply_bottom_friction)
 
     # Idealized surface forcing
-    tausx, tausx_ = domain.T.array(fill=tau_x)
-    tausy, tausy_ = domain.T.array(fill=tau_y)
+    tausx, tausx_ = domain.U.array(fill=tau_x)
+    tausy, tausy_ = domain.V.array(fill=tau_y)
     sp, sp_ = domain.T.array(fill=0.)
 
     dist_U = domain.distribute(sim.momentum.U_)
@@ -40,9 +54,10 @@ def test(name, periodic_x=False, periodic_y=False, tau_x=0., tau_y=0., timestep=
         sim.sealevel.update(timestep, sim.momentum.U_, sim.momentum.V_)
         sim.update_depth()
 
-    success = check_range('U', sim.momentum.U)
-    success = check_range('V', sim.momentum.V) and success
-    success = check_range('z', domain.T.z) and success
+    rho0 = 1025.
+    success = check_range('U', sim.momentum.U, target_value=None if apply_bottom_friction else 3600 * tau_x / rho0)
+    success = check_range('V', sim.momentum.V, target_value=None if apply_bottom_friction else 3600 * tau_y / rho0) and success
+    success = check_range('z', domain.T.z, target_value=0) and success
     return success
 
 if __name__ == '__main__':
