9c01447144fdc1f78d2d9797987a277d800918ce
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-06-17 09:12:07 +0100

    added nopass to u2d, v2d, w3d

--- src/operators/advection_schemes/advection_base.F90
+++ src/operators/advection_schemes/advection_base.F90
@@ -8,16 +8,15 @@ module advection_base
 
    type, abstract, public :: type_advection_base
    contains
-      procedure(u2d), deferred :: u2d
-      procedure(v2d), deferred :: v2d
-      procedure(w3d), deferred :: w3d
+      procedure(u2d), deferred, nopass :: u2d
+      procedure(v2d), deferred, nopass :: v2d
+      procedure(w3d), deferred, nopass :: w3d
    end type
 
    interface
-SUBROUTINE u2d(self,imin,imax,jmin,jmax,halo,umask,dxu,dyu,hu,u,tmask,iA,dt,h,f)
-   import type_advection_base, real64
+SUBROUTINE u2d(imin,imax,jmin,jmax,halo,umask,dxu,dyu,hu,u,tmask,iA,dt,h,f)
+   import real64
    ! Subroutine arguments
-   class (type_advection_base), intent(inout) :: self
    integer, intent(in) :: imin,imax,jmin,jmax
    integer, intent(in) :: halo(2)
 #define _A_  imin-halo(1):imax+halo(1),jmin-halo(2):jmax+halo(2)
@@ -34,10 +33,9 @@ SUBROUTINE u2d(self,imin,imax,jmin,jmax,halo,umask,dxu,dyu,hu,u,tmask,iA,dt,h,f)
 #undef _A_
 END SUBROUTINE
 
-SUBROUTINE v2d(self,imin,imax,jmin,jmax,halo,vmask,dxv,dyv,hv,v,tmask,iA,dt,h,f)
-   import type_advection_base, real64
+SUBROUTINE v2d(imin,imax,jmin,jmax,halo,vmask,dxv,dyv,hv,v,tmask,iA,dt,h,f)
+   import real64
    ! Subroutine arguments
-   class (type_advection_base), intent(inout) :: self
    integer, intent(in) :: imin,imax,jmin,jmax
    integer, intent(in) :: halo(2)
 #define _A_  imin-halo(1):imax+halo(1),jmin-halo(2):jmax+halo(2)
@@ -54,10 +52,9 @@ SUBROUTINE v2d(self,imin,imax,jmin,jmax,halo,vmask,dxv,dyv,hv,v,tmask,iA,dt,h,f)
 #undef _A_
 END SUBROUTINE
 
-SUBROUTINE w3d(self,imin, imax, jmin, jmax, kmax, w , tmask, dt, h, f)
-   import type_advection_base, real64
+SUBROUTINE w3d(imin, imax, jmin, jmax, kmax, w , tmask, dt, h, f)
+   import real64
    ! Subroutine arguments
-   class (type_advection_base), intent(inout) :: self
    integer, intent(in) :: imin, imax, jmin, jmax, kmax
    real(real64), intent(in) :: w(imin-1:imax+1,jmin-1:jmax+1,0:kmax)
    integer, intent(in) :: tmask(imin-1:imax+1,jmin-1:jmax+1)
