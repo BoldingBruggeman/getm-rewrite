425ff9d79f9fe2b50bcd645e1c9102f6d6e34f00
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-06-19 11:46:25 +0100

    domain: initialize glob attribute

--- python/pygetm/domain.py
+++ python/pygetm/domain.py
@@ -248,6 +248,7 @@ class Domain(_pygetm.Domain):
         assert lat is not None or f is not None, 'Either lat of f must be provided to determine the Coriolis parameter.'
 
         self.field_manager: Optional[output.FieldManager] = None
+        self.glob: Optional['Domain'] = None
 
         halo = 2
 
