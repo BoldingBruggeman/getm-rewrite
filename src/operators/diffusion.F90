f0616e6ef0e8cb63d7b7b3f660ee94d069e8e7cb
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-11-11 06:25:35 +0000

    vertical diffusion now takes nuh at internal interfaces (surface/bottom sliced out)

--- src/operators/diffusion.F90
+++ src/operators/diffusion.F90
@@ -144,7 +144,7 @@ MODULE SUBROUTINE vertical_diffusion_calculate(self,dt,cnpar,mask,dzo,dzn,molecu
       do j=self%jmin,self%jmax
          do i=self%imin,self%imax
             if (mask(i,j) ==  1) then
-               x = 2._real64*dt*(nuh(i,j,k+1)+molecular)
+               x = 2._real64*dt*(nuh(i,j,k)+molecular)
                self%auxo(i,j,k)=(1-cnpar)*x/(dzo(i,j,k+1)+dzo(i,j,k))
                self%auxn(i,j,k)=   cnpar *x/(dzn(i,j,k+1)+dzn(i,j,k))
             end if
