161e8a70d671b5f748697a67dfeccaa9d0b18efd
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-11-17 11:49:07 +0100

    north sea setup tweaks

--- python/examples/north_sea.py
+++ python/examples/north_sea.py
@@ -13,7 +13,7 @@ era_path = os.path.join(igotm_data_dir, 'ERA-interim/2016.nc')
 
 domain = pygetm.legacy.domain_from_topo(os.path.join(getm_setups_dir, 'NorthSea/Topo/NS6nm.v01.nc'), nlev=30, z0_const=0.001)
 pygetm.legacy.load_bdyinfo(domain, os.path.join(getm_setups_dir, 'NorthSea/bdyinfo.dat'))
-sim = pygetm.Simulation(domain, runtype=4, advection_scheme=1, fabm='../../extern/fabm/testcases/fabm-jrc-med_ergom.yaml')
+sim = pygetm.Simulation(domain, runtype=4, advection_scheme=1, gotm=os.path.join(getm_setups_dir, 'NorthSea/gotmturb.nml'), fabm='../../extern/fabm/testcases/fabm-jrc-med_ergom.yaml')
 
 #sim.input_manager.debug_nc_reads()
 
@@ -21,8 +21,9 @@ sim.logger.info('Setting up output')
 output = sim.output_manager.add_netcdf_file('north_sea.nc', interval=60)
 output.request(('u10', 'v10', 'sp', 't2m', 'd2m', 'tcc'))
 output.request(('U', 'V'))
-output.request('zt')
-output.request(('dpdx', 'dpdy', 'tausxu', 'tausyv'))
+output.request(('zt', 'Dt'))
+output.request(('dpdx', 'dpdy', 'tausxu', 'tausyv', 'u_taus'))
+output.request(('temp', 'salt', 'nuh', 'NN'))
 output.request(('med_ergom_o2', 'med_ergom_OFL'))
 
 sim.logger.info('Setting up ERA meteorological forcing')
@@ -36,11 +37,12 @@ sim.airsea.v10.set(pygetm.input.from_nc(era_path, 'v10', **era_kwargs))
 
 sim.logger.info('Setting up TPXO tidal boundary forcing')
 tpxo_dir = os.path.join(igotm_data_dir, 'TPXO9')
-bdy_lon = domain.T.lon[domain.bdy_j, domain.bdy_i]
-bdy_lat = domain.T.lat[domain.bdy_j, domain.bdy_i]
-sim.zbdy.set(pygetm.input.tpxo.get(bdy_lon, bdy_lat, root=tpxo_dir), on_grid=True)
-sim.bdyu.set(pygetm.input.tpxo.get(bdy_lon, bdy_lat, variable='u', root=tpxo_dir), on_grid=True)
-sim.bdyv.set(pygetm.input.tpxo.get(bdy_lon, bdy_lat, variable='v', root=tpxo_dir), on_grid=True)
+bdy_lon = domain.T.lon.all_values[domain.bdy_j, domain.bdy_i]
+bdy_lat = domain.T.lat.all_values[domain.bdy_j, domain.bdy_i]
+if domain.open_boundaries:
+    sim.zbdy.set(pygetm.input.tpxo.get(bdy_lon, bdy_lat, root=tpxo_dir), on_grid=True)
+    sim.bdyu.set(pygetm.input.tpxo.get(bdy_lon, bdy_lat, variable='u', root=tpxo_dir), on_grid=True)
+    sim.bdyv.set(pygetm.input.tpxo.get(bdy_lon, bdy_lat, variable='v', root=tpxo_dir), on_grid=True)
 
 sim.logger.info('Setting up FABM dependencies that GETM does not provide')
 sim.get_fabm_dependency('downwelling_photosynthetic_radiative_flux').set(0)
