b2178d754bf5d0ad38eaac11d29ebeb559006743
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2022-05-27 16:38:47 +0100

    applied PEP8 to mixing and density modules

--- python/pygetm/density.py
+++ python/pygetm/density.py
@@ -4,23 +4,32 @@ import numpy
 
 from . import core
 from . import _pygetm
-from .constants import *
+from .constants import INTERFACES
 import pygsw
 
 # Below we package up all equation-of-state/TEOS10 methods in a class.
-# This allows users to substitute other methods (e..g, alternative equation of state methods)
-# by inheriting from this class and overriding methos.
+# This allows users to substitute other methods (e..g, alternative equation of state
+# methods) by inheriting from this class and overriding methods.
+
 
 class Density:
     @staticmethod
-    def get_buoyancy_frequency(SA: core.Array, ct: core.Array, p: Optional[core.Array]=None, out: Optional[core.Array]=None) -> core.Array:
-        """Calculate the square of the buoyancy frequency at layer interfaces from absolute salinity, conservative temperature and pressure at the layer centers.
+    def get_buoyancy_frequency(
+        SA: core.Array,
+        ct: core.Array,
+        p: Optional[core.Array] = None,
+        out: Optional[core.Array] = None,
+    ) -> core.Array:
+        """Calculate the square of the buoyancy frequency at layer interfaces from
+        absolute salinity, conservative temperature and pressure at the layer centers.
         
         Args:
             SA: absolute salinity
             ct: conservative temperature (degrees Celsius)
-            p: pressure (dbar). If not provided, the water depth in m will be used as approximate pressure.
-            out: array to store buoyancy frequency result in. If not provided, a new array will be created.
+            p: pressure (dbar). If not provided, the water depth in m will be used as
+                approximate pressure.
+            out: array to store buoyancy frequency result in. If not provided,
+                a new array will be created.
 
         Returns:
             array with values for the square of the buoyancy frequency (s-2)
@@ -33,18 +42,34 @@ class Density:
         if p is None:
             p = _pygetm.thickness2center_depth(SA.grid.mask, SA.grid.hn)
         assert p.grid is SA.grid
-        pygsw.nsquared(SA.grid.mask.all_values, SA.grid.hn.all_values, SA.all_values, ct.all_values, p.all_values, SA.grid.lat.all_values, out.all_values[1:-1, :, :])
+        pygsw.nsquared(
+            SA.grid.mask.all_values,
+            SA.grid.hn.all_values,
+            SA.all_values,
+            ct.all_values,
+            p.all_values,
+            SA.grid.lat.all_values,
+            out.all_values[1:-1, :, :],
+        )
         return out
 
     @staticmethod
-    def get_density(SA: core.Array, ct: core.Array, p: Optional[core.Array]=None, out: Optional[core.Array]=None) -> core.Array:
-        """Calculate in-situ density from absolute salinity and conservative temperature. Inputs can be 2D or 3D.
+    def get_density(
+        SA: core.Array,
+        ct: core.Array,
+        p: Optional[core.Array] = None,
+        out: Optional[core.Array] = None,
+    ) -> core.Array:
+        """Calculate in-situ density from absolute salinity and conservative
+        temperature. Inputs can be 2D or 3D.
         
         Args:
             SA: absolute salinity
             ct: conservative temperature (degrees Celsius)
-            p: pressure (dbar). If not provided, the water depth in m will be used as approximate pressure.
-            out: array to store density result in. If not provided, a new array will be created.
+            p: pressure (dbar). If not provided, the water depth in m will be used as
+                approximate pressure.
+            out: array to store density result in. If not provided,
+                a new array will be created.
         
         Returns:
             array with density values (kg m-3)
@@ -56,17 +81,26 @@ class Density:
         if p is None:
             p = _pygetm.thickness2center_depth(SA.grid.mask, SA.grid.hn)
         assert p.grid is SA.grid
-        pygsw.rho(SA.all_values.ravel(), ct.all_values.ravel(), p.all_values.ravel(), out.all_values.ravel())
+        pygsw.rho(
+            SA.all_values.ravel(),
+            ct.all_values.ravel(),
+            p.all_values.ravel(),
+            out.all_values.ravel(),
+        )
         return out
 
     @staticmethod
-    def get_potential_temperature(SA: core.Array, ct: core.Array, out: Optional[core.Array]=None) -> core.Array:
-        """Calculate potential temperature from absolute salinity and conservative temperature. Inputs can be 2D or 3D.
+    def get_potential_temperature(
+        SA: core.Array, ct: core.Array, out: Optional[core.Array] = None
+    ) -> core.Array:
+        """Calculate potential temperature from absolute salinity and conservative
+        temperature. Inputs can be 2D or 3D.
         
         Args:
             SA: absolute salinity
             ct: conservative temperature (degrees Celsius)
-            out: array to store potential temperature result in. If not provided, a new array will be created.
+            out: array to store potential temperature result in. If not provided, a new
+                array will be created.
         
         Returns:
             array with potential temperature values (degrees Celsius)
@@ -75,19 +109,23 @@ class Density:
         if out is None:
             out = SA.grid.array(z=SA.z)
         assert out.grid is SA.grid and out.shape == SA.shape
-        pygsw.pt_from_ct(SA.all_values.ravel(), ct.all_values.ravel(), out.all_values.ravel())
+        pygsw.pt_from_ct(
+            SA.all_values.ravel(), ct.all_values.ravel(), out.all_values.ravel()
+        )
         return out
 
     @staticmethod
-    def convert_ts(sp: core.Array, pt: core.Array, p: core.Array=None) -> None:
-        """Convert practical salinity and potential temperature to absolute salinity and conservative temperature.
-        The conversion happens in-place: absolute salinity will replace practical salinity, conservative temperature
-        (degrees Celsius) will replace potential temperature.
+    def convert_ts(sp: core.Array, pt: core.Array, p: core.Array = None) -> None:
+        """Convert practical salinity and potential temperature to absolute salinity
+        and conservative temperature. The conversion happens in-place: absolute salinity
+        will replace practical salinity, conservative temperature (degrees Celsius)
+        will replace potential temperature.
         
         Args:
             sp: practical salinity (PSU)
             pt: potential temperature (degrees Celsius)
-            p: pressure (dbar). If not provided, the water depth in m will be used as approximate pressure.
+            p: pressure (dbar). If not provided, the water depth in m will be used as
+                approximate pressure.
         """
         assert sp.grid is pt.grid
         if p is None:
@@ -96,7 +134,13 @@ class Density:
         out = numpy.empty_like(sp.all_values)
         lon = numpy.broadcast_to(sp.grid.lon.all_values, out.shape)
         lat = numpy.broadcast_to(sp.grid.lat.all_values, out.shape)
-        pygsw.sa_from_sp(lon.ravel(), lat.ravel(), p.all_values.ravel(), sp.all_values.ravel(), out.ravel())
+        pygsw.sa_from_sp(
+            lon.ravel(),
+            lat.ravel(),
+            p.all_values.ravel(),
+            sp.all_values.ravel(),
+            out.ravel(),
+        )
         sp.all_values[...] = out
         pygsw.ct_from_pt(sp.all_values.ravel(), pt.all_values.ravel(), out.ravel())
         pt.all_values[...] = out
--- python/pygetm/mixing.py
+++ python/pygetm/mixing.py
@@ -6,52 +6,123 @@ import numpy
 from . import core
 from . import domain
 from . import _pygotm
-from .constants import *
+from .constants import INTERFACES, FILL_VALUE, ZERO_GRADIENT
+
 
 class Turbulence:
     """Base class that provides the turbulent viscosity ``num`` and diffusivity ``nuh``.
-    When using this class directly, viscosity and diffusivity are prescribed, not calculated.
-    In this case, both default to zero; assign to ``num``/``nuh`` or call ``num.set``/``nuh.set`` to change this."""
+    When using this class directly, viscosity and diffusivity are prescribed, not
+    calculated. In this case, both default to zero; assign to ``num``/``nuh`` or call
+    ``num.set``/``nuh.set`` to change this."""
+
     def initialize(self, grid: domain.Grid):
         self.grid = grid
         self.logger = grid.domain.root_logger.getChild(self.__class__.__name__)
-        self.nuh = grid.array(z=INTERFACES, name='nuh', units='m2 s-1', long_name='turbulent diffusivity of heat', fill_value=FILL_VALUE, attrs={'_part_of_state': True})
-        self.num = grid.array(z=INTERFACES, name='num', units='m2 s-1', long_name='turbulent diffusivity of momentum', fill_value=FILL_VALUE, attrs={'_part_of_state': True})
-        self.nuh.fill(0.)
-        self.num.fill(0.)
+        self.nuh = grid.array(
+            z=INTERFACES,
+            name="nuh",
+            units="m2 s-1",
+            long_name="turbulent diffusivity of heat",
+            fill_value=FILL_VALUE,
+            attrs={"_part_of_state": True},
+        )
+        self.num = grid.array(
+            z=INTERFACES,
+            name="num",
+            units="m2 s-1",
+            long_name="turbulent diffusivity of momentum",
+            fill_value=FILL_VALUE,
+            attrs={"_part_of_state": True},
+        )
+        self.nuh.fill(0.0)
+        self.num.fill(0.0)
 
-    def advance(self, timestep: float, ustar_s: core.Array, ustar_b: core.Array, z0s: core.Array, z0b: core.Array, NN: core.Array, SS: core.Array):
+    def advance(
+        self,
+        timestep: float,
+        ustar_s: core.Array,
+        ustar_b: core.Array,
+        z0s: core.Array,
+        z0b: core.Array,
+        NN: core.Array,
+        SS: core.Array,
+    ):
         pass
 
+
 class GOTM(Turbulence):
-    """Calculate the turbulent viscosity ``num`` and diffusivity ``nuh`` using the `General Ocean Turbulence Model (GOTM)
-    <https://gotm.net>`_.
+    """Calculate the turbulent viscosity ``num`` and diffusivity ``nuh`` using
+    the `General Ocean Turbulence Model (GOTM) <https://gotm.net>`_.
    """
-    def __init__(self, nml_path: Optional[str]=None):
+
+    def __init__(self, nml_path: Optional[str] = None):
         super().__init__()
         self.nml_path = nml_path
 
     def initialize(self, grid: domain.Grid):
         super().initialize(grid)
-        self.mix = _pygotm.Mixing(grid.nz, b'' if self.nml_path is None else self.nml_path.encode('ascii'))
+
+        self.mix = _pygotm.Mixing(
+            grid.nz, b"" if self.nml_path is None else self.nml_path.encode("ascii")
+        )
         self.nuh.fill(self.mix.nuh[:, numpy.newaxis, numpy.newaxis])
         self.num.fill(self.mix.num[:, numpy.newaxis, numpy.newaxis])
-        self.tke = grid.array(fill=self.mix.tke[:, numpy.newaxis, numpy.newaxis], z=INTERFACES, name='tke', units='m2 s-2', long_name='turbulent kinetic energy', attrs={'_part_of_state': True})
-        self.tkeo = grid.array(fill=self.mix.tkeo[:, numpy.newaxis, numpy.newaxis], z=INTERFACES, name='tkeo', units='m2 s-2', long_name='turbulent kinetic energy at previous timestep', attrs={'_part_of_state': True})
-        self.eps = grid.array(fill=self.mix.eps[:, numpy.newaxis, numpy.newaxis], z=INTERFACES, name='eps', units='m2 s-3', long_name='energy dissipation rate', attrs={'_part_of_state': True})
-        self.L = grid.array(fill=self.mix.L[:, numpy.newaxis, numpy.newaxis], z=INTERFACES, name='L', units='m', long_name='turbulence length scale', attrs={'_part_of_state': True})
+        self.tke = grid.array(
+            fill=self.mix.tke[:, numpy.newaxis, numpy.newaxis],
+            z=INTERFACES,
+            name="tke",
+            units="m2 s-2",
+            long_name="turbulent kinetic energy",
+            attrs={"_part_of_state": True},
+        )
+        self.tkeo = grid.array(
+            fill=self.mix.tkeo[:, numpy.newaxis, numpy.newaxis],
+            z=INTERFACES,
+            name="tkeo",
+            units="m2 s-2",
+            long_name="turbulent kinetic energy at previous timestep",
+            attrs={"_part_of_state": True},
+        )
+        self.eps = grid.array(
+            fill=self.mix.eps[:, numpy.newaxis, numpy.newaxis],
+            z=INTERFACES,
+            name="eps",
+            units="m2 s-3",
+            long_name="energy dissipation rate",
+            attrs={"_part_of_state": True},
+        )
+        self.L = grid.array(
+            fill=self.mix.L[:, numpy.newaxis, numpy.newaxis],
+            z=INTERFACES,
+            name="L",
+            units="m",
+            long_name="turbulence length scale",
+            attrs={"_part_of_state": True},
+        )
         self.log()
 
     def log(self):
-        for l in itertools.chain(_pygotm.stdout, _pygotm.stderr):
-            l = l.rstrip()
-            if l:
-                self.logger.info(l)
+        for line in itertools.chain(_pygotm.stdout, _pygotm.stderr):
+            line = line.rstrip()
+            if line:
+                self.logger.info(line)
 
-    def advance(self, timestep: float, ustar_s: core.Array, ustar_b: core.Array, z0s: core.Array, z0b: core.Array, NN: core.Array, SS: core.Array):
-        """Update turbulent quantities and calculate turbulent diffusivity nuh and turbulent viscosity,
-        using surface and bottom friction velocity (ustar_s, ustar_b, both in m s-1), surface and bottom hydrodynamic roughness (z0s, z0b, both in m)
-        squared buoyancy frequency (NN in s-2), and squared shear frequency (SS in s-2)."""
+    def advance(
+        self,
+        timestep: float,
+        ustar_s: core.Array,
+        ustar_b: core.Array,
+        z0s: core.Array,
+        z0b: core.Array,
+        NN: core.Array,
+        SS: core.Array,
+    ):
+        """Update turbulent quantities and calculate turbulent diffusivity nuh and
+        turbulent viscosity, using surface and bottom friction velocity (ustar_s,
+        ustar_b, both in m s-1), surface and bottom hydrodynamic roughness (z0s, z0b,
+        both in m) squared buoyancy frequency (NN in s-2), and squared shear frequency
+        (SS in s-2).
+        """
         assert ustar_s.grid is self.grid and ustar_s.ndim == 2
         assert ustar_b.grid is self.grid and ustar_b.ndim == 2
         assert z0s.grid is self.grid and z0s.ndim == 2
@@ -60,14 +131,36 @@ class GOTM(Turbulence):
         assert SS.grid is self.grid and SS.z == INTERFACES
 
         nz, ny, nx = self.grid.hn.all_values.shape
-        self.mix.turbulence_3d(nx, ny, nz, 2, nx - 2, 2, ny - 2, timestep, self.grid.mask.all_values,
-            self.grid.hn.all_values, self.grid.D.all_values, ustar_s.all_values, ustar_b.all_values,
-            z0s.all_values, z0b.all_values, NN.all_values, SS.all_values,
-            self.tke.all_values, self.tkeo.all_values, self.eps.all_values, self.L.all_values, self.num.all_values, self.nuh.all_values)
+        self.mix.turbulence_3d(
+            nx,
+            ny,
+            nz,
+            2,
+            nx - 2,
+            2,
+            ny - 2,
+            timestep,
+            self.grid.mask.all_values,
+            self.grid.hn.all_values,
+            self.grid.D.all_values,
+            ustar_s.all_values,
+            ustar_b.all_values,
+            z0s.all_values,
+            z0b.all_values,
+            NN.all_values,
+            SS.all_values,
+            self.tke.all_values,
+            self.tkeo.all_values,
+            self.eps.all_values,
+            self.L.all_values,
+            self.num.all_values,
+            self.nuh.all_values,
+        )
 
         # Take viscosity at open boundary from nearest interior point
-        # Viscosity (at T points) needs to be valid at open boundary points to so it can be interpolated to inward-adjacent U/V points.
-        # However, it cannot be computed as the shear frequency SS is not available at the boundary.
+        # Viscosity (at T points) needs to be valid at open boundary points to so it
+        # can be interpolated to inward-adjacent U/V points. However, it cannot be
+        # computed as the shear frequency SS is not available at the boundary.
         self.num.update_boundary(ZERO_GRADIENT)
 
         self.log()
