9097d91d1fc0d8a113127c123a3b54039c6af447
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2022-07-11 16:17:56 +0100

    applied pep8 to test_parallel_advection.py

--- python/tests/test_parallel_advection.py
+++ python/tests/test_parallel_advection.py
@@ -9,24 +9,46 @@ import pygetm.parallel
 import matplotlib.pyplot
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--debug', action='store_true', help='diagnose individual subdomains')
-parser.add_argument('-i', '--interval', type=int, help='number of timesteps between plots', default=10)
-parser.add_argument('--nmax', type=int, help='total number of timesteps', default=None)
-parser.add_argument('--nrow', type=int, help='number of rows in subdomain division', default=2)
-parser.add_argument('--ncol', type=int, help='number of columns in subdomain division', default=2)
-parser.add_argument('-n', type=int, help='number of revolutions', default=5)
-parser.add_argument('--noplot', action='store_false', dest='plot', help='skip plotting (useful for performance testing)')
-parser.add_argument('--profile', action='store_true', help='use profiler to time function calls')
-parser.add_argument('-o', '--output', help='NetCDF file to save result to')
+parser.add_argument(
+    "-d", "--debug", action="store_true", help="diagnose individual subdomains"
+)
+parser.add_argument(
+    "-i", "--interval", type=int, help="number of timesteps between plots", default=10
+)
+parser.add_argument("--nmax", type=int, help="total number of timesteps", default=None)
+parser.add_argument(
+    "--nrow", type=int, help="number of rows in subdomain division", default=2
+)
+parser.add_argument(
+    "--ncol", type=int, help="number of columns in subdomain division", default=2
+)
+parser.add_argument("-n", type=int, help="number of revolutions", default=5)
+parser.add_argument(
+    "--noplot",
+    action="store_false",
+    dest="plot",
+    help="skip plotting (useful for performance testing)",
+)
+parser.add_argument(
+    "--profile", action="store_true", help="use profiler to time function calls"
+)
+parser.add_argument("-o", "--output", help="NetCDF file to save result to")
 args = parser.parse_args()
 
-Lx, Ly = 100., 100.
+Lx, Ly = 100.0, 100.0
 nx, ny, nlev = 612, 600, 1
 
 tiling = pygetm.parallel.Tiling(args.nrow, args.ncol)
 rank = tiling.rank
 
-subdomain = pygetm.domain.create_cartesian(np.linspace(-Lx/2, Lx/2, nx), np.linspace(-Ly/2, Ly/2, ny), nlev, H=1, f=0., tiling=tiling)
+subdomain = pygetm.domain.create_cartesian(
+    np.linspace(-Lx / 2, Lx / 2, nx),
+    np.linspace(-Ly / 2, Ly / 2, ny),
+    nlev,
+    H=1,
+    f=0.0,
+    tiling=tiling,
+)
 halo = subdomain.halo
 subdomain.initialize(runtype=pygetm.BAROTROPIC_2D)
 outman = pygetm.output.OutputManager(subdomain.fields, rank=rank)
@@ -34,13 +56,13 @@ outman = pygetm.output.OutputManager(subdomain.fields, rank=rank)
 f_glob = None
 if subdomain.glob:
     # Set up global domain and initial tracer field
-    f_glob = subdomain.glob.T.array(fill=0.)
-    f_glob[int(0.2 * ny):int(0.4 * ny), int(0.2 * nx):int(0.4 * nx)] = 5.
+    f_glob = subdomain.glob.T.array(fill=0.0)
+    f_glob[int(0.2 * ny) : int(0.4 * ny), int(0.2 * nx) : int(0.4 * nx)] = 5.0
 
 # Set up velocities
 period = 600
 omega = 2 * np.pi / period
-cfl = 1.
+cfl = 1.0
 umax = omega * Lx / 2
 dt_cfl = cfl * min(Lx / nx, Ly / ny) / umax
 Nmax = args.n * round(2 * np.pi / omega / dt_cfl)
@@ -49,19 +71,21 @@ timestep = tmax / Nmax
 
 # Calculate u and v
 # Note that this only sets values in the interior of the domain.
-# Therefore, a halo exchange is needed to ensure u and v are also valid in the innermost halo strip (needed for advection scheme)
+# Therefore, a halo exchange is needed to ensure u and v are also valid
+# in the innermost halo strip (needed for advection scheme)
 u = -omega * subdomain.U.y
 v = omega * subdomain.V.x
-u[(2 * subdomain.U.x / Lx)**2 + (2 * subdomain.U.y / Ly)**2 >= 1] = 0.
-v[(2 * subdomain.V.x / Lx)**2 + (2 * subdomain.V.y / Ly)**2 >= 1] = 0.
+u[(2 * subdomain.U.x / Lx) ** 2 + (2 * subdomain.U.y / Ly) ** 2 >= 1] = 0.0
+v[(2 * subdomain.V.x / Lx) ** 2 + (2 * subdomain.V.y / Ly) ** 2 >= 1] = 0.0
 u.update_halos()
 v.update_halos()
 
 if args.nmax:
     Nmax = args.nmax
 
-# Set up tracer field for subdomain, wrap it for halo updates and MPI-scatter it from root node
-f = subdomain.T.array(fill=0., name='tracer')
+# Set up tracer field for subdomain, wrap it for halo updates
+# and MPI-scatter it from root node
+f = subdomain.T.array(fill=0.0, name="tracer")
 f.scatter(f_glob)
 
 # Gather and plot global velocities
@@ -69,16 +93,25 @@ u_glob = u.gather()
 v_glob = v.gather()
 if u_glob is not None and args.plot:
     fig, ax = matplotlib.pyplot.subplots()
-    u_destag, v_destag = u_glob.interp(u_glob.grid.domain.T), v_glob.interp(u_glob.grid.domain.T)
-    ax.quiver(u_destag[::10, ::10], v_destag[::10, ::10], angles='xy')
-    fig.savefig('vel.png')
+    u_destag, v_destag = (
+        u_glob.interp(u_glob.grid.domain.T),
+        v_glob.interp(u_glob.grid.domain.T),
+    )
+    ax.quiver(u_destag[::10, ::10], v_destag[::10, ::10], angles="xy")
+    fig.savefig("vel.png")
 
 if args.debug:
     # Plot local velocities
     fig, ax = matplotlib.pyplot.subplots()
     u_destag, v_destag = u.interp(subdomain.T), v.interp(subdomain.T)
-    ax.quiver(subdomain.T.x[::10, ::10], subdomain.T.y[::10, ::10], u_destag[::10, ::10], v_destag[::10, ::10], angles='xy')
-    fig.savefig('vel_%i.png' % rank)
+    ax.quiver(
+        subdomain.T.x[::10, ::10],
+        subdomain.T.y[::10, ::10],
+        u_destag[::10, ::10],
+        v_destag[::10, ::10],
+        angles="xy",
+    )
+    fig.savefig("vel_%i.png" % rank)
 
     # Set up figure for plotting tracer per subdomain
     fig_sub, ax_sub = matplotlib.pyplot.subplots()
@@ -93,10 +126,11 @@ if f_glob is not None and args.plot:
 
 if args.output:
     ncf = outman.add_netcdf_file(args.output, interval=10)
-    ncf.request('tracer')
+    ncf.request("tracer")
 outman.start()
 
 advect = pygetm.operators.Advection(subdomain.T, scheme=4)
+
 
 def main():
     ifig = 0
@@ -104,40 +138,58 @@ def main():
     for i in range(Nmax):
         if i % args.interval == 0:
             if rank == 0:
-                print('time step %i of %i' % (i, Nmax), flush=True)
+                print("time step %i of %i" % (i, Nmax), flush=True)
 
             if args.debug:
-                # Print tracer max along boundaries, inside and outside halo 
-                print(i, rank, 'inside', f[0, :].max(), f[-1, :].max(), f[:, 0].max(), f[:, -1].max(), flush=True)
-                print(i, rank, 'outside', rank, f.all_values[halo-1, :].max(), f.all_values[-halo, :].max(), f.all_values[:, halo-1].max(), f.all_values[:, -halo].max(), flush=True)
+                # Print tracer max along boundaries, inside and outside halo
+                print(
+                    i,
+                    rank,
+                    "inside",
+                    f[0, :].max(),
+                    f[-1, :].max(),
+                    f[:, 0].max(),
+                    f[:, -1].max(),
+                    flush=True,
+                )
+                print(
+                    i,
+                    rank,
+                    "outside",
+                    rank,
+                    f.all_values[halo - 1, :].max(),
+                    f.all_values[-halo, :].max(),
+                    f.all_values[:, halo - 1].max(),
+                    f.all_values[:, -halo].max(),
+                    flush=True,
+                )
 
                 # Plot local tracer field
                 pc_sub.set_array(f[...].ravel())
-                fig_sub.savefig('subadv_%i_%04i.png' % (rank, ifig))
+                fig_sub.savefig("subadv_%i_%04i.png" % (rank, ifig))
 
             # Gather and plot global tracer field
             if args.plot:
                 f.gather(out=f_glob)
                 if f_glob is not None:
                     pc.set_array(f_glob[...].ravel())
-                    fig.savefig('adv_%04i.png' % ifig)
+                    fig.savefig("adv_%04i.png" % ifig)
 
             ifig += 1
 
         # Advect
         advect(u, v, timestep, f)
 
-        # Update halos
-        f.update_halos()
-
         outman.save(i * timestep, i)
 
     duration = timeit.default_timer() - start
-    print('Time spent in loop: %.4f s' % duration)
-    print('%.4f ms per iteration' % (1000 * duration / Nmax,))
+    print("Time spent in loop: %.4f s" % duration)
+    print("%.4f ms per iteration" % (1000 * duration / Nmax,))
+    outman.close()
+
 
 if args.profile:
-    cProfile.run('main()', sort='tottime')
+    cProfile.run("main()", sort="tottime")
 else:
     main()
 
