8732920a249475f7015a14ac819e74a6f22ed215
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2022-07-12 12:54:34 +0100

    refactored test_2d_transport.py to use unittest

--- python/tests/test_2d_transport.py
+++ python/tests/test_2d_transport.py
@@ -1,186 +1,92 @@
-import sys
-import argparse
+import unittest
 
 import numpy as np
 
 import pygetm
 
 
-def check_range(name, values, rtol=1e-12, atol=1e-12, target_value=None):
-    values = np.asarray(values)
-    print("  %s... " % name, end="", flush=True)
-    absrange = values.max() - values.min()
-    mean = values.mean()
-    crit = rtol * abs(mean) + atol
-    valid = absrange < crit
-    result = "OK" if valid else "FAILED because field is not homogeneous"
-    relrange = 0 if absrange == 0 else absrange / abs(mean)
-    if valid and target_value is not None:
-        valid = abs(mean - target_value) < rtol * abs(target_value) + atol
-        if not valid:
-            result = "FAILED to match target value %.6g" % target_value
-    print(
-        "%s (mean %.6g, spread %.6g, relative spread %.6g)"
-        % (result, mean, absrange, relrange)
-    )
-    if not valid:
-        plot("%s.png" % name, values)
-    return valid
+class Test2DTransport(unittest.TestCase):
+    def check_range(self, name, values, rtol=1e-12, atol=1e-12, target_value=None):
+        values = np.asarray(values)
+        absrange = values.max() - values.min()
+        mean = values.mean()
+        crit = rtol * abs(mean) + atol
+        self.assertLess(absrange, crit, "%s is not homogeneous" % name)
+        if target_value is not None:
+            self.assertLess(
+                abs(mean - target_value),
+                rtol * abs(target_value) + atol,
+                "failed to match target value %s" % target_value,
+            )
 
+    def test_all(self):
+        taus = [0.0, 0.01, -0.01]
+        for apply_bottom_friction in [True, False]:
+            for tau_x in taus:
+                for tau_y in taus:
+                    with self.subTest(tau_x=tau_x, tau_y=tau_y):
+                        self._test(
+                            tau_x=tau_x,
+                            tau_y=tau_y,
+                            periodic_x=tau_x != 0.0,
+                            periodic_y=tau_y != 0.0,
+                            apply_bottom_friction=apply_bottom_friction,
+                        )
 
-def plot(name, values):
-    import matplotlib.pyplot
-
-    fig, ax = matplotlib.pyplot.subplots()
-    pc = ax.pcolormesh(values)
-    fig.colorbar(pc)
-    fig.savefig(name, dpi=300)
-
-
-def test(
-    name,
-    periodic_x: bool = False,
-    periodic_y: bool = False,
-    tau_x: float = 0.0,
-    tau_y: float = 0.0,
-    timestep: float = 10.0,
-    ntime: int = 360,
-    apply_bottom_friction: bool = False,
-):
-    print("%s, tau_x = %s, tau_y = %s..." % (name, tau_x, tau_y), flush=True)
-
-    # Set up rectangular domain (all points unmasked)
-    domain = pygetm.domain.create_cartesian(
-        500.0 * np.arange(100),
-        500.0 * np.arange(30),
-        1,
-        f=0,
-        H=50,
-        periodic_x=periodic_x,
-        periodic_y=periodic_y,
-    )
-    sim = pygetm.Simulation(
-        domain,
-        runtype=pygetm.BAROTROPIC_2D,
-        apply_bottom_friction=apply_bottom_friction,
-    )
-
-    # Idealized surface forcing
-    tausx = domain.U.array(fill=tau_x)
-    tausy = domain.V.array(fill=tau_y)
-    sp = domain.T.array(fill=0.0)
-
-    for istep in range(ntime):
-        sim.update_surface_pressure_gradient(domain.T.z, sp)
-        sim.momentum.advance_depth_integrated(
-            timestep, tausx, tausy, sim.dpdx, sim.dpdy
+    def _test(
+        self,
+        periodic_x=False,
+        periodic_y=False,
+        tau_x=0.0,
+        tau_y=0.0,
+        timestep=10.0,
+        ntime=360,
+        apply_bottom_friction=False,
+    ):
+        # Set up rectangular domain (all points unmasked)
+        domain = pygetm.domain.create_cartesian(
+            500.0 * np.arange(100),
+            500.0 * np.arange(30),
+            1,
+            f=0,
+            H=50,
+            periodic_x=periodic_x,
+            periodic_y=periodic_y,
+            logger=pygetm.parallel.get_logger(level="ERROR"),
         )
-        sim.advance_surface_elevation(timestep, sim.momentum.U, sim.momentum.V, sim.fwf)
-        sim.domain.update_depth()
-
-    success = check_range(
-        "U",
-        sim.momentum.U,
-        target_value=None
-        if apply_bottom_friction
-        else ntime * timestep * tau_x / pygetm.RHO0,
-    )
-    success = (
-        check_range(
-            "V",
-            sim.momentum.V,
-            target_value=None
-            if apply_bottom_friction
-            else ntime * timestep * tau_y / pygetm.RHO0,
+        sim = pygetm.Simulation(
+            domain,
+            runtype=pygetm.BAROTROPIC_2D,
+            apply_bottom_friction=apply_bottom_friction,
         )
-        and success
-    )
-    success = check_range("z", domain.T.z, target_value=0) and success
-    return success
+
+        # Idealized surface forcing
+        tausx = domain.U.array(fill=tau_x)
+        tausy = domain.V.array(fill=tau_y)
+        sp = domain.T.array(fill=0.0)
+
+        for _ in range(ntime):
+            sim.update_surface_pressure_gradient(domain.T.z, sp)
+            sim.momentum.advance_depth_integrated(
+                timestep, tausx, tausy, sim.dpdx, sim.dpdy
+            )
+            sim.advance_surface_elevation(
+                timestep, sim.momentum.U, sim.momentum.V, sim.fwf
+            )
+            sim.domain.update_depth()
+
+        u_ref = ntime * timestep * tau_x / pygetm.RHO0
+        v_ref = ntime * timestep * tau_y / pygetm.RHO0
+        u_tgt = None if apply_bottom_friction else u_ref
+        v_tgt = None if apply_bottom_friction else v_ref
+        self.check_range(
+            "U", sim.momentum.U, target_value=u_tgt,
+        )
+        self.check_range(
+            "V", sim.momentum.V, target_value=v_tgt,
+        )
+        self.check_range("z", domain.T.z, target_value=0.0)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--apply_bottom_friction", action="store_true")
-    args = parser.parse_args()
-
-    success = test(
-        "Periodic in x",
-        periodic_x=True,
-        tau_x=0.01,
-        apply_bottom_friction=args.apply_bottom_friction,
-    )
-    success = (
-        test(
-            "Periodic in x",
-            periodic_x=True,
-            tau_x=-0.01,
-            apply_bottom_friction=args.apply_bottom_friction,
-        )
-        and success
-    )
-    success = (
-        test(
-            "Periodic in y",
-            periodic_y=True,
-            tau_y=0.01,
-            apply_bottom_friction=args.apply_bottom_friction,
-        )
-        and success
-    )
-    success = (
-        test(
-            "Periodic in y",
-            periodic_y=True,
-            tau_y=-0.01,
-            apply_bottom_friction=args.apply_bottom_friction,
-        )
-        and success
-    )
-    success = (
-        test(
-            "Periodic in x and y",
-            periodic_x=True,
-            periodic_y=True,
-            tau_x=0.01,
-            tau_y=0.01,
-            apply_bottom_friction=args.apply_bottom_friction,
-        )
-        and success
-    )
-    success = (
-        test(
-            "Periodic in x and y",
-            periodic_x=True,
-            periodic_y=True,
-            tau_x=0.01,
-            tau_y=-0.01,
-            apply_bottom_friction=args.apply_bottom_friction,
-        )
-        and success
-    )
-    success = (
-        test(
-            "Periodic in x and y",
-            periodic_x=True,
-            periodic_y=True,
-            tau_x=-0.01,
-            tau_y=0.01,
-            apply_bottom_friction=args.apply_bottom_friction,
-        )
-        and success
-    )
-    success = (
-        test(
-            "Periodic in x and y",
-            periodic_x=True,
-            periodic_y=True,
-            tau_x=-0.01,
-            tau_y=-0.01,
-            apply_bottom_friction=args.apply_bottom_friction,
-        )
-        and success
-    )
-    if not success:
-        sys.exit(1)
-
+    unittest.main()
