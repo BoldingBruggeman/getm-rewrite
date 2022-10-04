98d1b85501736df98fd60b016eea7740810d2ef1
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2022-10-04 11:01:40 +0100

    fixed tmrlx in north_sea.py

--- python/examples/north_sea.py
+++ python/examples/north_sea.py
@@ -31,7 +31,7 @@ def create_domain(
 
     if use_boundaries:
         pygetm.legacy.load_bdyinfo(domain, os.path.join(setup_dir, "bdyinfo.dat"))
-        domain.open_boundaries.tmrlx = True
+        domain.open_boundaries.sponge.tmrlx = True
     if use_rivers:
         pygetm.legacy.load_riverinfo(domain, os.path.join(setup_dir, "riverinfo.dat"))
 
