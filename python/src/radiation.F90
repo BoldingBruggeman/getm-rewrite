7c7b205e76accb7c803fe13d25d14a8c8eb849bc
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-04-21 11:00:01 +0100

    removed exponential_profile_2band_interfaces

--- python/src/radiation.F90
+++ python/src/radiation.F90
@@ -53,34 +53,4 @@ contains
       end do
    end subroutine
 
-   subroutine c_exponential_profile_2band_interfaces(nx, ny, nz, istart, istop, jstart, jstop, mask, &
-      h, f1, kc1, kc2, initial, up, out) bind(c)
-      integer(c_int), intent(in), value :: nx, ny, nz, istart, istop, jstart, jstop, up
-      integer(c_int), intent(in)        :: mask(nx, ny)
-      real(c_double), intent(in)        :: h(nx, ny, nz), f1(nx, ny), kc1(nx, ny), kc2(nx, ny), initial(nx, ny)
-      real(c_double), intent(inout)     :: out(nx, ny, 0:nz)
-
-      integer :: k, kstart, kstop, kstep, h_offset
-      real(c_double) :: cumh(nx, ny)
-
-      if (up /= 0) then
-         kstart = 0
-         kstop = nz
-         kstep = 1
-         h_offset = 0
-      else
-         kstart = nz
-         kstop = 0
-         kstep = -1
-         h_offset = 1
-      end if
-
-      where (mask /= 0) out(:,:,kstart) = initial
-      cumh = 0._c_double
-      do k = kstart + kstep, kstop, kstep
-         cumh = cumh + h(:,:,k+h_offset)
-         where (mask /= 0) out(:,:,k) = initial * (f1  * exp(-kc1 * cumh) + (1._c_double - f1) * exp(-kc2 * cumh))
-      end do
-   end subroutine
-
 end module
