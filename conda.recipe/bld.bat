572808fc95da304b7546325402e415ddf6792202
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-04-24 10:38:23 +0100

    updated recipe to check out clean code and run full test suite

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,9 +1,8 @@
 mkdir build
 cd build
-cmake -DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_BUILD_TYPE=Release %RECIPE_DIR%\..\python
-cmake --build . --config Release --parallel %CPU_COUNT%
-cmake --install .
+cmake -DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_BUILD_TYPE=Release %SRC_DIR%\python || exit /b
+cmake --build . --config Release --parallel %CPU_COUNT% || exit /b
+cmake --install . || exit /b
 cd ..
 rmdir /S /Q build
 
-if errorlevel 1 exit 1
--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release $RECIPE_DIR/../python
+cmake -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release $SRC_DIR/python
 cmake --build . --config Release --parallel $CPU_COUNT
 cmake --install .
 cd ..
