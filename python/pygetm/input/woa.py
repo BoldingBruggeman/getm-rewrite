4f61cec58fc852e36ff522dde1eb62f1027e89ed
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-06-27 17:44:31 +0100

    woa: default to OPeNDAP access, improved filling, document arguments

--- python/pygetm/input/woa.py
+++ python/pygetm/input/woa.py
@@ -1,9 +1,11 @@
 import argparse
-from typing import Optional
+from typing import Optional, Literal
 import datetime
+import logging
 
 import netCDF4
-import xarray
+import scipy.spatial
+import numpy as np
 
 
 def copyNcVariable(
@@ -41,65 +43,142 @@ def copyNcVariable(
     return ncvarnew
 
 
-def convert(woapaths: str, outfile: str, name: Optional[str]) -> str:
-    print('Combining annual mean and monthly values...')
+def convert(
+    name: Literal["t", "s", "n", "p", "i", "o"],
+    outfile: str,
+    decade: Optional[
+        Literal[
+            "decav", "5564", "6574", "7584", "8594", "95A4", "A5B7", "decav81B0", "all"
+        ]
+    ] = None,
+    grid: Optional[Literal["01", "04", "5d"]] = None,
+    template: Optional[str] = None,
+    logger: Optional[logging.Logger] = None,
+) -> str:
+    logger = logger or logging.getLogger()
+    if grid is None:
+        grid = "04" if name in "ts" else "01"
+
+    long_grid = {"04": "0.25", "01": "1.00", "5d": "5deg"}[grid]
+    long_name = {
+        "t": "temperature",
+        "s": "salinity",
+        "n": "nitrate",
+        "p": "phosphate",
+        "i": "silicate",
+        "o": "oxygen",
+    }[name]
+    if decade is None:
+        decade = "decav" if name in "ts" else "all"
+    if template is None:
+        template = f"https://www.ncei.noaa.gov/thredds-ocean/dodsC/ncei/woa/{long_name}/{decade}/{long_grid}/woa18_{decade}_{name}%02i_{grid}.nc"
+    varname = name + "_an"
+    logger.info(f"Combining annual mean and monthly values for {name}...")
     with netCDF4.Dataset(outfile, "w", format="NETCDF3_64BIT_OFFSET") as ncout:
         ncout.set_fill_off()
         ncout.createDimension("time", 12)
-        print("  - mean climatology")
-        with netCDF4.Dataset(woapaths % 0) as ncin:
-            if name is None:
-                for name in ncin.variables:
-                    if name.endswith("_an"):
-                        break
+        logger.info("  - mean climatology")
+        with netCDF4.Dataset(template % 0) as ncin:
             ncin.set_auto_maskandscale(False)
             copyNcVariable(ncin["depth"], ncout)
             copyNcVariable(ncin["lon"], ncout)
             copyNcVariable(ncin["lat"], ncout)
-            ncvar = ncin[name]
+            ncvar = ncin[varname]
             andata = ncvar[...]
             dim = ncvar.dimensions[1]
         nctime = ncout.createVariable("time", float, ("time",))
         time_ref = datetime.datetime(2000, 1, 1)
         nctime.units = f"days since {time_ref}"
         nctime.calendar = "standard"
-        print("  - monthly data:")
+        logger.info("  - monthly data:")
         for imonth in range(1, 13):
-            print("    - %i" % imonth)
+            logger.info("    - %i" % imonth)
             dt = datetime.datetime(2000, imonth, 15, 12)
-            with netCDF4.Dataset(woapaths % imonth) as ncin:
+            with netCDF4.Dataset(template % imonth) as ncin:
                 nctime[imonth - 1] = (dt - time_ref).days
                 ncin.set_auto_maskandscale(False)
-                ncvar = ncin[name]
+                ncvar = ncin[varname]
                 if imonth == 1:
                     ncvar_mo = copyNcVariable(
                         ncvar,
                         ncout,
                         dimensions=("time", dim, "lat", "lon"),
                         copy_data=False,
-                        name=name,
+                        name=varname,
                     )
                     ncvar_mo[:, ncvar.shape[1] :, :, :] = andata[
                         :, ncvar.shape[1] :, :, :
                     ]
                 ncvar_mo[imonth - 1, : ncvar.shape[1], :, :] = ncvar[0, ...]
-    return name
+    return varname
 
-def fill(path: str, name: str):
-    print('Filling missing values...')
-    ds = xarray.open_dataset(path)
-    da = ds[name].interpolate_na(dim='lat', method='nearest')
-    da.to_netcdf(path)
+
+def fill(path: str, varname: str, logger: Optional[logging.Logger] = None):
+    logger = logger or logging.getLogger()
+    logger.info("Filling missing values...")
+
+    with netCDF4.Dataset(path, "r+") as nc:
+        ncvar = nc[varname]
+        indices = np.indices(ncvar.shape[1:])
+        masked = np.ma.getmaskarray(ncvar[0, :, :, :])
+        unmasked = ~masked
+        masked_ind = np.array(indices[:, masked].T, dtype=float)
+        unmasked_ind = np.array(indices[:, unmasked].T, dtype=float)
+        logger.info(f"  - building KDTree")
+        tree = scipy.spatial.cKDTree(unmasked_ind)
+        logger.info(f"  - finding nearest neighbors")
+        dist, inearest = tree.query(masked_ind, workers=-1)
+        for itime in range(ncvar.shape[0]):
+            logger.info(f"  - writing time {itime}")
+            data = ncvar[itime, ...]
+            data[masked] = data[unmasked][inearest]
+            ncvar[itime, :, :, :] = data
+
 
 if __name__ == "__main__":
     import argparse
 
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger()
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("source")
-    parser.add_argument("outfile")
-    parser.add_argument("--name", default=None)
-    parser.add_argument("--fill", action='store_true')
+    parser.add_argument(
+        "name",
+        choices="tsnpio",
+        help="character identifying the variable to operate on (t=temperature, s=salinity, n=nitrate, p=phosphate, i=silicate, o=oxygen)",
+    )
+    parser.add_argument(
+        "outfile", nargs="?", help="output file, defaults to <variable character>.nc"
+    )
+    parser.add_argument(
+        "--template",
+        default=None,
+        help="python format string (%% based) specifying the NetCDF files to operate on. It must contain %%02i as placeholder for the month",
+    )
+    parser.add_argument(
+        "--grid",
+        choices=("01", "04", "5d"),
+        help="Resolution to use (only used if --template is not provided), defaults to highest available",
+    )
+    parser.add_argument(
+        "--decade",
+        default=None,
+        help="Decade to use (only used if --template is not provided), defaults to average over entire time period",
+    )
+    parser.add_argument(
+        "--fill", action="store_true", help="whether to fill missing values"
+    )
     args = parser.parse_args()
-    name = convert(args.source, args.outfile, name=args.name)
+    if args.outfile is None:
+        args.outfile = f"{args.name}.nc"
+    varname = convert(
+        args.name,
+        args.outfile,
+        grid=args.grid,
+        decade=args.decade,
+        template=args.template,
+        logger=logger,
+    )
     if args.fill:
-        fill(args.outfile, name)
+        fill(args.outfile, varname, logger=logger)
+    logger.info(f"Results saved to {args.outfile}")
