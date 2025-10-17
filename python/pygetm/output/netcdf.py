5ee270d10a6ac52ae5fadef0f5e636d31e0c9c62
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-10-17 14:49:39 +0100

    refactor netcdf writing to skip unused files and coordinates

--- python/pygetm/output/netcdf.py
+++ python/pygetm/output/netcdf.py
@@ -1,4 +1,4 @@
-from typing import Optional, Mapping, Dict, List
+from typing import Optional, Mapping, Dict, List, Tuple
 import os.path
 import logging
 import datetime
@@ -11,6 +11,98 @@ from . import File
 from . import operators
 import pygetm.core
 import pygetm._pygetm
+
+
+def _create_file(path: str, format: str) -> netCDF4.Dataset:
+    # Create the NetCDF file (note: some NetCDF engines such as h5netcdf
+    # do not support the format argument)
+    nc = netCDF4.Dataset(path, "w", format=format)
+    now = datetime.datetime.now()
+    cmdline = " ".join(sys.argv)
+    nc.history = f"{now:%Y-%m-%d %H:%M:%S} {cmdline}"
+    nc.source = f"pygetm {pygetm._pygetm.get_version()}"
+    return nc
+
+
+def _create_dimensions(
+    nc: netCDF4.Dataset, fields: Mapping[str, operators.Base]
+) -> Tuple[bool, bool]:
+    needs_time = False
+    needs_time_bounds = False
+    for output_name, field in fields.items():
+        for dim, length in zip(field.dims, field.shape):
+            if dim not in nc.dimensions:
+                assert length > 0
+                nc.createDimension(dim, length)
+            elif length != nc.dimensions[dim].size:
+                raise Exception(
+                    f"Error adding {output_name} with shape {field.shape}:"
+                    f" existing dimension {dim} has incompatible length"
+                    f" {nc.dimensions[dim].size} (needs {length})"
+                )
+        needs_time |= bool(field.time_varying)
+        needs_time_bounds |= "time: mean" in field.attrs.get("cell_methods", "")
+
+    if needs_time_bounds:
+        nc.createDimension("nv", 2)
+    if needs_time:
+        nc.createDimension("time", None)
+    return needs_time, needs_time_bounds
+
+
+def _add_time_coordinate(
+    nc: netCDF4.Dataset,
+    time: Optional[cftime.datetime],
+    seconds_passed: float,
+    time_reference: Optional[cftime.datetime],
+) -> Tuple[netCDF4.Variable, float]:
+    nctime = nc.createVariable("time", float, ("time",))
+    nctime.axis = "T"
+    if time is not None:
+        nctime.units = f"seconds since {time_reference:%Y-%m-%d %H:%M:%S}"
+        nctime.calendar = time.calendar
+        time_offset = (time - time_reference).total_seconds() - seconds_passed
+    else:
+        nctime.units = "s"
+        nctime.standard_name = "time"
+        time_offset = 0.0
+    return nctime, time_offset
+
+
+def _add_time_bounds(nc: netCDF4.Dataset, nctime: netCDF4.Variable) -> netCDF4.Variable:
+    nctime_bnds = nc.createVariable("time_bnds", float, ("time", "nv"))
+    nctime.bounds = "time_bnds"
+    for att in ("units", "calendar"):
+        if hasattr(nctime, att):
+            setattr(nctime_bnds, att, getattr(nctime, att))
+    return nctime_bnds
+
+
+def _add_variable(
+    nc: netCDF4.Dataset, name: str, field: operators.Base, **kwargs
+) -> netCDF4.Variable:
+    dims = field.dims
+    if field.time_varying:
+        dims = ("time",) + dims
+    ncvar = nc.createVariable(
+        name, field.dtype, dims, fill_value=field.fill_value, **kwargs
+    )
+
+    # Use try-except for set_auto_maskandscale because only some NetCDF
+    # engines support it (netCDF4 does, h5netcdf.legacyapi does not)
+    try:
+        ncvar.set_auto_maskandscale(False)
+    except AttributeError:
+        pass
+
+    # Variable attributes
+    ncvar.expression = field.expression
+    for att, value in field.attrs.items():
+        setattr(ncvar, att, value)
+    if field.coordinates:
+        ncvar.coordinates = " ".join(field.coordinates)
+
+    return ncvar
 
 
 class NetCDFFile(File):
@@ -61,7 +153,8 @@ class NetCDFFile(File):
         self.compression = compression
         self._field2nc: Dict[operators.Base, netCDF4.Variable] = {}
         self._varying_fields: List[operators.Base] = []
-        self.nctime_bnds = None
+        self.nctime: Optional[netCDF4.Variable] = None
+        self.nctime_bnds: Optional[netCDF4.Variable] = None
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.path!r})"
@@ -73,90 +166,31 @@ class NetCDFFile(File):
         default_time_reference: Optional[cftime.datetime],
     ):
         if self.is_root or self.sub:
-            # Create the NetCDF file (note: some NetCDF engines such as h5netcdf
-            # do not support the format argument)
-            self.nc = netCDF4.Dataset(self.path, "w", format=self.format)
-            now = datetime.datetime.now()
-            cmdline = " ".join(sys.argv)
-            self.nc.history = f"{now:%Y-%m-%d %H:%M:%S} {cmdline}"
-            self.nc.source = f"pygetm {pygetm._pygetm.get_version()}"
-
-            # Add dimensions
-            for output_name, field in self.fields.items():
-                for dim, length in zip(field.dims, field.shape):
-                    if dim not in self.nc.dimensions:
-                        if length > 0:
-                            self.nc.createDimension(dim, length)
-                    elif length != self.nc.dimensions[dim].size:
-                        raise Exception(
-                            f"Error adding {output_name} with shape {field.shape}:"
-                            f" existing dimension {dim} has incompatible length"
-                            f" {self.nc.dimensions[dim].size} (need {length})"
-                        )
-            self.nc.createDimension("time", None)
-
-            # Add time coordinate
-            self.nctime = self.nc.createVariable("time", float, ("time",))
-            self.nctime.axis = "T"
-            if time is not None:
-                time_reference = self.time_reference or default_time_reference
-                self.nctime.units = f"seconds since {time_reference:%Y-%m-%d %H:%M:%S}"
-                self.nctime.calendar = time.calendar
-                self.time_offset = (
-                    time - time_reference
-                ).total_seconds() - seconds_passed
-            else:
-                self.nctime.units = "s"
-                self.nctime.standard_name = "time"
-
-            # Create variables for all requested fields
-            needs_time_bounds = False
+            included_fields = {}
             for output_name, field in self.fields.items():
                 if 0 in field.shape:
                     self._logger.warning(
-                        f"Skipping output of {output_name} because it contains no data"
+                        f"Skipping {output_name} because it contains no data"
                         f" (shape={field.shape})"
                     )
-                    continue
-                dims = field.dims
-                if field.time_varying:
-                    dims = ("time",) + dims
-                ncvar = self.nc.createVariable(
-                    output_name,
-                    field.dtype,
-                    dims,
-                    fill_value=field.fill_value,
-                    compression=self.compression,
-                )
+                else:
+                    included_fields[output_name] = field
 
-                # Use try-except for set_auto_maskandscale because only some NetCDF
-                # engines support it (netCDF4 does, h5netcdf.legacyapi does not)
-                try:
-                    ncvar.set_auto_maskandscale(False)
-                except AttributeError:
-                    pass
-
-                # Variable attributes
-                ncvar.expression = field.expression
-                for att, value in field.attrs.items():
-                    setattr(ncvar, att, value)
-                if field.coordinates:
-                    ncvar.coordinates = " ".join(field.coordinates)
-                needs_time_bounds |= "time: mean" in field.attrs.get("cell_methods", "")
-
-                self._field2nc[field] = ncvar
-
-            # Add coordinate variable for time bounds if needed
-            if needs_time_bounds:
-                self.nc.createDimension("nv", 2)
-                self.nctime_bnds = self.nc.createVariable(
-                    "time_bnds", float, ("time", "nv")
-                )
-                self.nctime.bounds = "time_bnds"
-                for att in ("units", "calendar"):
-                    if hasattr(self.nctime, att):
-                        setattr(self.nctime_bnds, att, getattr(self.nctime, att))
-                self.previous_time_coord = self.time_offset + seconds_passed
+            if included_fields:
+                self.nc = _create_file(self.path, self.format)
+                has_time, has_time_bounds = _create_dimensions(self.nc, included_fields)
+                if has_time:
+                    time_reference = self.time_reference or default_time_reference
+                    self.nctime, self.time_offset = _add_time_coordinate(
+                        self.nc, time, seconds_passed, time_reference
+                    )
+                    if has_time_bounds:
+                        self.nctime_bnds = _add_time_bounds(self.nc, self.nctime)
+                        self.previous_time_coord = self.time_offset + seconds_passed
+                for output_name, field in included_fields.items():
+                    self._field2nc[field] = _add_variable(
+                        self.nc, output_name, field, compression=self.compression
+                    )
 
         for field in self.fields.values():
             if field.time_varying:
@@ -167,15 +201,15 @@ class NetCDFFile(File):
                 field.get(self._field2nc.get(field))
 
     def save_now(self, seconds_passed: float, time: Optional[cftime.datetime]):
-        # Update time coordinate(s)
-        if self.nc is not None:
+        # Update time coordinate(s), if used
+        if self.nctime is not None:
             time_coord = self.time_offset + seconds_passed
             self.nctime[self.itime] = time_coord
             if self.nctime_bnds is not None:
                 self.nctime_bnds[self.itime, :] = [self.previous_time_coord, time_coord]
                 self.previous_time_coord = time_coord
 
-        # Update all time-varying fields
+        # Update time-varying fields
         for field in self._varying_fields:
             field.get(self._field2nc.get(field), slice_spec=(self.itime,))
 
