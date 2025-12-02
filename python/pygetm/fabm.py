6627a58bc7a38b2d49187f47e45e45752f2994af
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-12-02 10:02:41 +0000

    fabm: restore ability to specify model configuration as mapping

--- python/pygetm/fabm.py
+++ python/pygetm/fabm.py
@@ -1,5 +1,6 @@
 import logging
-from typing import Optional, MutableMapping, Iterable, Union
+from typing import Any, Optional, Union
+from collections.abc import Iterable, Mapping, MutableMapping
 import os
 import contextlib
 
@@ -14,18 +15,41 @@ from .constants import TimeVarying
 
 
 class FABM:
+    """Interface to the Framework for Aquatic Biogeochemical Models (FABM)."""
+
     def __init__(
         self,
-        path: Union[os.PathLike[str], str] = "fabm.yaml",
+        path: Union[os.PathLike[str], str, Mapping[str, Any]] = "fabm.yaml",
         repair: bool = True,
         bioshade_feedback: bool = False,
         libname: str = os.path.join(os.path.dirname(__file__), "fabm"),
         time_varying: TimeVarying = TimeVarying.MACRO,
         squeeze: bool = False,
     ):
-        self.path = str(path)
+        """Initialize FABM interface.
+
+        Args:
+            path: Path to FABM configuration file, in YAML format, or a mapping
+                representing the parsed content of such a file.
+            repair: Whether to attempt to repair invalid FABM state values
+                by clipping them to valid ranges.
+            bioshade_feedback: Whether to obtain the attenuation coefficient
+                for photosynthetic active radiation from FABM. This will then
+                contribute to light absorption in the radiation module, which
+                in turn affects the heat distribution in the water column.
+            libname: Path to FABM shared library, excluding any platform-specific
+                prefix/suffix
+            time_varying: Model step at which FABM variables will be updated.
+                FABM arrays will be flagged with this attribute to tell the
+                output manager when they are updated.
+            squeeze: Whether FABM's internal representation of arrays has all
+                singleton dimensions (with length 1) squeezed out.
+        """
+        if not isinstance(path, Mapping):
+            path = str(path)
+        self.path = path
         self.repair = repair
-        self.bioshade_feedback: bool = bioshade_feedback
+        self.bioshade_feedback = bioshade_feedback
         self.libname = libname
         self.time_varying = time_varying
         self.squeeze = squeeze
