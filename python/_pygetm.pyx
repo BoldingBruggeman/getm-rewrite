d3147e3adba0abeab4f9b673c38e65eac8f46854
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-12-12 13:48:36 +0000

    optimize assignment to all_values

--- python/_pygetm.pyx
+++ python/_pygetm.pyx
@@ -88,7 +88,7 @@ cdef class Array:
     @all_values.setter
     def all_values(self, value):
         if value is not self._array:
-            self._array[...] = value
+            numpy.PyArray_CopyObject(self._array, value)
 
     cdef wrap_c_array(self, grid, int source, void* obj, bytes name, register=True):
         cdef int grid_type
