82027ec7659ed3ff24d49da998843c5c2c4531cf
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2026-01-09 15:41:03 +0000

    drop redundant define

--- python/src/tridiagonal.F90
+++ python/src/tridiagonal.F90
@@ -32,14 +32,12 @@ subroutine c_tridiagonal(nx, ny, nz, halox, haloy, &
 
  !  Local variables
 #ifdef _USE_3D_
-#define _D2_ -halox+1:nx+halox,-haloy+1:ny+haloy
    real(c_double),allocatable :: auxo(:,:,:)
    real(c_double),allocatable :: auxn(:,:,:)
    real(c_double),allocatable :: a1(:,:,:)
    real(c_double),allocatable :: a2(:,:,:)
    real(c_double),allocatable :: a3(:,:,:)
    real(c_double),allocatable :: a4(:,:,:)
-#undef _D2_
 #else
    real(c_double) :: auxo(0:nz)
    real(c_double) :: auxn(0:nz)
